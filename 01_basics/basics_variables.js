690bfb0a16c7ba4254deeacfc2b55bcce7c0b89c
Shashank kale <145908026+shashank1749@users.noreply.github.com>
2025-08-26 15:13:02 +0000

    commit

---
+++ 01_basics/basics_variables.js
@@ -0,0 +1,8 @@
+const accountId=12342
+var accoutName="shashank"
+let password=""
+
+// accountName="vishal"
+// password="985308"
+
+console.table([accountId,accoutName,password])
---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,25 @@
+let score=NaN
+console.log(typeof(score));
+
+
+//Number()----> Values of other types can be converted to numbers using the Number() function.
+
+const newscore=Number(score);
+
+console.log(typeof(newscore));
+
+console.log(newscore)
+
+
+//"33"----> string->number  -->string convert into number
+//"33abs"---->number+world--> output is NaN
+//true----> true=1-->false=0---> true convert into 1 and --> number-->1
+//NaN------> conver into number --> output is 0
+
+
+let isLoggedIn=1;
+console.log(Boolean(isLoggedIn));
+
+//true=1   false=0
+//""-->false
+//"shashank"-->true
